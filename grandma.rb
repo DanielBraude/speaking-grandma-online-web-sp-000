8b1bc9500c8e541d5f9e4877e91199dd2e2da50d
Daniel Abraham Braude <daniboy1007@gmail.com>
2020-10-13 19:52:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!" )
-
+def speak_to_grandma(string)
+  if string == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
